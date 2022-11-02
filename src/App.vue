ed670a11754c61d32e54c9c1cacc15cce56e8ba8
jgoriginal <932274204@qq.com>
2022-11-02 13:38:12 +0800

    city页面根据meta隐藏tabBar标签栏

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
 <router-view />
-<tab-bar></tab-bar>
+<tab-bar v-if="!route.meta.hideTabBar" />
 </template>
 <script setup>
 import tabBar from '@/components/tabBar/tabBar.vue'
+import { useRoute } from 'vue-router';
+const route = useRoute()
 </script>
 <style scoped>
 </style>
---
+++ src/views/city/city.vue
@@ -0,0 +1,9 @@
+<template >
+  <div class="city">
+    city
+  </div>
+</template>
+<script setup>
+</script>
+<style lang="less" scoped>
+</style>
