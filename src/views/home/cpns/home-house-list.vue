60859f489ab7c324e3f95ad97aa3ef431558ef53
jgoriginal <932274204@qq.com>
2022-11-04 22:45:32 +0800

    搜索栏的布局与样式

--- src/views/home/cpns/home-house-list.vue
+++ src/views/home/cpns/home-house-list.vue
@@ -5,44 +5,16 @@
         <house-item-v3 v-if="item.discoveryContentType === 3" :item-data="item.data"/>
       </template>    
   </div>
-  <button @click="moreListClick">加载更多</button>
 </template>
 <script setup>
 import useHomeStore from '@/store/modules/home';
 import { storeToRefs } from 'pinia';
 import houseItemV9 from '@/components/house-item-v9/house-item-v9.vue';
 import houseItemV3 from '@/components/house-item-v3/house-item-v3.vue';
-import useScroll from '@/hooks/useScroll'
-import { watch,ref } from 'vue'
 // 房屋列表 : Pinia 里获取
 const homeStore = useHomeStore()
 const { houseList } = storeToRefs(homeStore)
 
-// 记载更多按钮
-const moreListClick = () => {
-  homeStore.fetchHouseList()
-}
-
-// 使用监听到达页面底部的  Hooks
-const {isReachBottom } = useScroll()
-watch(isReachBottom, (newValue) => {
-  if (newValue === true) {
-    homeStore.fetchHouseList().then(() => {
-      isReachBottom.value = false
-    })
-  }
-})
-// 搜索栏的 显示/隐藏
-const searchVisible = ref(false)
-// Hooks 获滚动的高度
-const { scrollTop } = useScroll()
-watch(scrollTop, (newValue) => {
-  if (newValue > 200) {
-    searchVisible.value = true
-  } else {
-    searchVisible.value = false
-  }
-})
 </script>
 <style lang="less" scoped>
   .house-list {
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,12 +4,18 @@
   <div class="banner">
     <img src="@/assets/img/home/banner.webp" alt="">
   </div>
-  <!--搜索框-->
+  <!--搜索盒子-->
   <home-search-box />
   <!--分类列表-->
   <home-categories />
   <!--房屋列表-->
   <home-house-list />
+  <!--搜索框(滑动页面会出现)-->
+  <Transition  name="slide">
+    <div class="search-wrapper" v-if="searchVisible">
+      <search-bar />
+    </div>
+  </Transition>
 </div>
 </template>
 <script setup>
@@ -18,11 +24,35 @@ import homeSearchBox from './cpns/home-search-box.vue';
 import homeCategories from './cpns/home-categories.vue';
 import useHomeStore from '@/store/modules/home';
 import homeHouseList from './cpns/home-house-list.vue';
+import useScroll from '@/hooks/useScroll'
+import { watch, ref } from 'vue'
+import searchBar from '@/components/search-bar/search-bar.vue';
 // 调用 action : 获取热门剪辑
 const homeStore = useHomeStore()
 homeStore.fetchHotSuggests()
 homeStore.fetchCategories()
 homeStore.fetchHouseList()
+
+// 使用监听到达页面底部的  Hooks
+const {isReachBottom } = useScroll()
+watch(isReachBottom, (newValue) => {
+  if (newValue === true) {
+    homeStore.fetchHouseList().then(() => {
+      isReachBottom.value = false
+    })
+  }
+})
+// 搜索栏的 显示/隐藏
+const searchVisible = ref(false)
+// Hooks 获滚动的高度
+const { scrollTop } = useScroll()
+watch(scrollTop, (newValue) => {
+  if (newValue > 350) {
+    searchVisible.value = true
+  } else {
+    searchVisible.value = false
+  }
+})
 </script>
 <style lang="less" scoped>
 .home {
@@ -32,5 +62,19 @@ homeStore.fetchHouseList()
   img {
     width:100%
   }
+}
+.search-wrapper {
+  position:fixed;
+  left:0;
+  top:0;
+  right:0;
+  padding:10px;
+  background-color: white;
+}
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.25s ease;
+}
+.slide-enter-from, .slide-leave-to {
+  transform:translateY(-100%);
 }
 </style>
