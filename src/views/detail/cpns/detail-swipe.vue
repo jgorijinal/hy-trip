eb932327e830a2d10afdc52d38449fa52e292e4a
jgoriginal <932274204@qq.com>
2022-11-05 21:01:25 +0800

    轮播图及自定义指示器的封装实现

--- src/views/detail/cpns/detail-swipe.vue
+++ src/views/detail/cpns/detail-swipe.vue
@@ -1,21 +1,51 @@
 <template>
   <div class="swipe">
     <van-swipe class="swipe-list" :autoplay="3000" indicator-color="white">
+      <!--轮播图图片 (v-for 循环)-->
       <template v-for="item in swipeData" :key="item.orderIndex">
         <van-swipe-item class="item">
           <img :src="item.url" alt="">
         </van-swipe-item>
       </template>
+      <!--自定义指示器-->
+      <template #indicator="{ active, total }">
+        <div class="indicator">
+          <template v-for="value,key,index in swipeGroup" :key="index">
+            <span class="indicator-item" :class="{active : Number(key) === swipeData[active]?.enumPictureCategory}">
+              <span class="text">{{getTitle(value[0].title)}}</span>
+              <span class="index" v-if=" Number(key) === swipeData[active]?.enumPictureCategory">
+                {{value.findIndex(item=> item === swipeData[active]) + 1}} / {{value.length}}
+              </span>
+            </span>
+          </template>
+        </div>
+      </template>
     </van-swipe>
   </div>
 </template>
 <script setup>
+// props 接受 : 轮播图的数据
 const props = defineProps({
   swipeData: {
     type: Array,
     default: () => [],
   },
 });
+console.log(props.swipeData)
+// 数据转化
+const swipeGroup = {}
+props.swipeData.forEach(item => {
+  let values = swipeGroup[item['enumPictureCategory']]
+  if (!values) {
+    values = []
+    swipeGroup[item['enumPictureCategory']] = values
+  }
+  values.push(item)
+})
+
+const getTitle = (str) => {
+  return str.replace('：','').replace('【','').replace('】','')
+}
 </script>
 <style lang="less" scoped>
 .swipe {
@@ -27,4 +57,22 @@ const props = defineProps({
     }
   }
 }
+.indicator {
+    position: absolute;
+    right: 5px;
+    bottom: 5px;
+    padding: 2px 5px;
+    font-size: 12px;
+    background: rgba(36, 36, 36, 0.6);
+    color:white;
+    &-item {
+      margin:0 6px;
+      &.active {
+        background-color: white;
+        color: black;
+        padding:1px 3px;
+        border-radius:4px;
+      }
+    }
+  }
 </style>
--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -18,17 +18,19 @@ import { getDetailInfos } from '@/service/modules/detail'
 import { ref, computed } from 'vue'
 import detailSwipe from './cpns/detail-swipe.vue';
 const router = useRouter()
+const route = useRoute()
+// 返回键
 const onClickLeft = () => {
   router.back()
 }
-
-const route = useRoute()
+// 总详情数据
 const detailInfos = ref({})
+// 发请求获取
 getDetailInfos(route.params.id).then(res => {
   detailInfos.value = res.data
   console.log(detailInfos.value)
 })
-
+// mainPart 数据
 const mainPart = computed(() => detailInfos.value.mainPart)
 
 </script>
