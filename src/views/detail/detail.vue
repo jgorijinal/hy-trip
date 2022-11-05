fe883e94add8c6a340bec2bb18f0cdf7b7ec89f3
jgoriginal <932274204@qq.com>
2022-11-05 15:33:41 +0800

    详情数据的请求在页面内部管理

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="detail">
-    {{$route.params.id}}
+    <van-nav-bar
+      title="房屋详情"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+    />
   </div>
 </template>
 <script setup>
+import { useRouter,useRoute } from 'vue-router';
+import { getDetailInfos } from '@/service/modules/detail'
+import { ref,computed } from 'vue'
+const router = useRouter()
+const onClickLeft = () => {
+  router.back()
+}
+
+const route = useRoute()
+const detailInfos = ref({})
+getDetailInfos(route.params.id).then(res => {
+  detailInfos.value = res.data
+  console.log(detailInfos.value)
+})
+
+const mainPart = computed(() => detailInfos.value.mainPart)
 
 </script>
 <style lang="less" scoped>
