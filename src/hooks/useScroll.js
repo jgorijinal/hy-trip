9f87ecd573db1c8ce237d3f6e21bfb727c15ec46
jgoriginal <932274204@qq.com>
2022-11-06 14:17:46 +0800

    useScroll 监听元素的滚动

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,7 +1,9 @@
 // 监听是否到达了页面的底部
 import { ref, onMounted, onUnmounted, onActivated, onDeactivated } from 'vue';
 import { throttle } from 'underscore'
-export default function useScroll() {
+export default function useScroll(elRef) {
+  console.log(elRef.value)
+  let el = window
   // 是否到达底部
   const isReachBottom = ref(false)
   const scrollHeight = ref(0)
@@ -9,25 +11,35 @@ export default function useScroll() {
   const scrollTop = ref(0)
 
   const scrollListener = throttle(() => {
-    scrollHeight.value = document.documentElement.scrollHeight
-    clientHeight.value = document.documentElement.clientHeight
-    scrollTop.value = document.documentElement.scrollTop
+    if(el === window) {
+      scrollHeight.value = document.documentElement.scrollHeight
+      clientHeight.value = document.documentElement.clientHeight
+      scrollTop.value = document.documentElement.scrollTop
+    } else {
+      scrollHeight.value = el.scrollHeight
+      clientHeight.value = el.clientHeight
+      scrollTop.value = el.scrollTop
+    }
     if (scrollHeight.value <= clientHeight.value + scrollTop.value) {
       isReachBottom.value = true
     }
   },200)
-  onMounted(() => {
-    window.addEventListener('scroll', scrollListener)
+  onMounted(() => { 
+    if(elRef){
+      el = elRef.value
+    }
+    console.log(el)
+    el.addEventListener('scroll', scrollListener)
   })
   onActivated(() => {
-    window.addEventListener('scroll', scrollListener)
+    el.addEventListener('scroll', scrollListener)
   })
   // 取消监听
   onUnmounted(() => {
-    window.removeEventListener('scroll', scrollListener)
+    el.removeEventListener('scroll', scrollListener)
   })
   onDeactivated(() => {
-    window.removeEventListener('scroll', scrollListener)
+    el.removeEventListener('scroll', scrollListener)
   })
   return {
     isReachBottom,clientHeight,scrollHeight,scrollTop
--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="detail">
+  <div class="detail" ref="detailRef">
     <van-nav-bar
       title="房屋详情"
       left-text="返回"
@@ -8,19 +8,19 @@
     />
     <!--轮播图-->
     <div class="main" v-if="mainPart">
-      <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics"/>
+      <detail-swipe  :swipe-data="mainPart.topModule.housePicture.housePics"/>
         <!--信息-->
-      <detail-info :top-infos="mainPart.topModule" />
+      <detail-info  :top-infos="mainPart.topModule" />
       <!--房屋设施-->
-      <detail-facility :house-facility="mainPart.dynamicModule.facilityModule.houseFacility" />
+      <detail-facility  :house-facility="mainPart.dynamicModule.facilityModule.houseFacility" />
       <!--房东介绍-->
       <detail-landlord :landlord="mainPart.dynamicModule.landlordModule"/>
       <!--评论-->
-      <detail-comment :comment="mainPart.dynamicModule.commentModule"/>
+      <detail-comment  :comment="mainPart.dynamicModule.commentModule"/>
       <!--预定须知-->
-      <detail-notice :order-rules="mainPart.dynamicModule.rulesModule.orderRules"/>
+      <detail-notice  :order-rules="mainPart.dynamicModule.rulesModule.orderRules"/>
       <!--百度地图-->
-      <detail-map :position="mainPart.dynamicModule.positionModule"/>
+      <detail-map  :position="mainPart.dynamicModule.positionModule"/>
       <!--介绍-->
       <detail-intro :price-intro="mainPart.introductionModule"/>
     </div>
@@ -28,12 +28,20 @@
       <img src="@/assets/img/detail/icon_ensure.png" alt="">
       <div class="text">弘源旅途, 永无止境!</div>
     </div>
+      <!--标签页(滑动时出现)-->
+  <div class="tab-control" v-if="tabVisible">
+    <van-tabs v-model:active="tabActive" @click-tab="clickTab">
+      <template v-for="title,index in tabTitles" :key="index">
+        <van-tab :title="title"></van-tab>
+      </template>
+    </van-tabs>
+  </div>
   </div>
 </template>
 <script setup>
 import { useRouter,useRoute } from 'vue-router';
 import { getDetailInfos } from '@/service/modules/detail'
-import { ref, computed } from 'vue'
+import { ref, computed,watch,onMounted } from 'vue'
 import detailSwipe from './cpns/detail-01-swipe.vue';
 import detailInfo from './cpns/detail-02-info.vue';
 import detailFacility from './cpns/detail-03-facility.vue';
@@ -42,6 +50,7 @@ import detailComment from './cpns/detail-05-comment.vue';
 import detailNotice from './cpns/detail-06-notice.vue';
 import detailMap from './cpns/detail-07-map.vue';
 import detailIntro from './cpns/detail-08-intro.vue';
+import  useScroll from '@/hooks/useScroll'
 const router = useRouter()
 const route = useRoute()
 // 返回键
@@ -58,10 +67,29 @@ getDetailInfos(route.params.id).then(res => {
 // mainPart 数据
 const mainPart = computed(() => detailInfos.value.mainPart)
 
+// 显示/隐藏 标签页 
+const tabVisible = ref(false)
+const tabActive = ref(0)
+// tab 标题
+const tabTitles = ['描述', '设施', '房东', '评论', '须知', '周边']
+const detailRef = ref()
+const { scrollTop } = useScroll(detailRef) 
+watch(scrollTop, (newTop) => {
+  console.log(newTop)
+  if (newTop > 300) {
+    tabVisible.value = true
+  } else {
+    tabVisible.value = false
+  }
+})
+const clickTab = (obj) => {
+  
+}
 </script>
 <style lang="less" scoped>
 .detail {
   height: 100vh;
+  overflow: auto;
 }
 .footer {
   display: flex;
@@ -79,5 +107,12 @@ const mainPart = computed(() => detailInfos.value.mainPart)
     font-size: 12px;
     color: #7688a7;
   }
+}
+.tab-control {
+  position:fixed;
+  left:0;
+  right:0;
+  top:0;
+  z-index:99;
 }
 </style>
